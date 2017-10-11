4138396e7bba88c7867726626f352f534bc95ec3
Rafał Majkowski <majkipl@gmail.com>
2017-10-11 08:15:04 +0200

    modify for auth

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
             WhenceSeeder::class,
             ProductSeeder::class,
             ContestSeeder::class,
-            PromotionSeeder::class
+            PromotionSeeder::class,
+            UserSeeder::class
         ]);
     }
 }
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Enums\UserRole;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [];
+
+        $data[] = [
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'role' => UserRole::ADMIN,
+            'email_verified_at' => now(),
+            'password' => Hash::make('asd123'), // password
+            'remember_token' => Str::random(10)
+        ];
+
+        User::insert($data);
+    }
+}
